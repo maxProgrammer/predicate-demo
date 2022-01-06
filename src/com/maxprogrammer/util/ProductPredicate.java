42ef4f732aff76a25321e64daf5ab73123ea2437
maxProgrammer <maxwneto.poo@gmail.com>
2022-01-06 15:22:12 -0300

    revised predicate demo using lambda

---
+++ src/com/maxprogrammer/util/ProductPredicate.java
@@ -0,0 +1,13 @@
+package com.maxprogrammer.util;
+
+import com.maxprogrammer.entities.Product;
+
+import java.util.function.Predicate;
+
+public class ProductPredicate implements Predicate<Product> {
+
+    @Override
+    public boolean test(Product p) {
+        return p.getPrice() >= 100.0;
+    }
+}
